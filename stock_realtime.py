2059c009601353589fa55854ab9606b3269806da
Yuichi <yuichiok@buffalo.edu>
2017-08-05 11:50:13 +0900

    + stock_realtime

---
+++ stock_realtime.py
@@ -0,0 +1,26 @@
+import urllib3  # works fine with Python 2.7.9 (not 3.4.+)
+import json
+import time
+
+def fetchPreMarket(symbol, exchange):
+    http = urllib3.PoolManager()
+    link = "http://finance.google.com/finance/info?client=ig&q="
+    url = link+"%s:%s" % (exchange, symbol)
+    u = http.request('GET',url)
+    content = u.read()
+    data = json.loads(content[3:])
+    info = data[0]
+    t = str(info["elt"])    # time stamp
+    l = float(info["l"])    # close price (previous trading day)
+    p = float(info["el"])   # stock price in pre-market (after-hours)
+    return (t,l,p)
+
+	
+p0 = 0
+while True:
+    t, l, p = fetchPreMarket("AAPL","NASDAQ")
+    if(p!=p0):
+        p0 = p
+        print("%s\t%.2f\t%.2f\t%+.2f\t%+.2f%%" % (t, l, p, p-l,
+                                                 (p/l-1)*100.))
+    time.sleep(60)
